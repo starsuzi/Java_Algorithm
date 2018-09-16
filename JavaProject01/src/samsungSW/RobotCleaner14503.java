6f49f6283b6eafbf7d4b7a41502136f1b5e8e4b8
정소영 <starsuzi@naver.com>
2018-09-17 02:39:03 +0900

    dfs

--- JavaProject01/src/samsungSW/RobotCleaner14503.java
+++ JavaProject01/src/samsungSW/RobotCleaner14503.java
@@ -43,6 +43,7 @@ public class RobotCleaner14503 {
 		//1.방문체크
 		if(map[currentY][currentX] == 0) {
 			clean(currentX, currentY);
+			System.out.println("(currentX, currentY) ("+currentX+","+currentY+") clean");
 		}
 
 		//2.연결된길
@@ -51,42 +52,72 @@ public class RobotCleaner14503 {
 		int newDir = left(currentX, currentY, dir);
 		int targetY = currentY + dy[newDir];
 		int targetX = currentX +dx[newDir];
+		
+		//3.갈수있는길
+		if(targetY >= 0 && targetY<N && targetX >= 0 && targetX <M ) {
+			if(map[targetY][targetX] == 0 ) {
 
-			//3.갈수있는길
-			if(targetY >= 0 && targetY<N && targetX >= 0 && targetX <M ) {
-				if(map[targetY][targetX] == 0 ) {
-					//4.간다	
-					dfs(targetX, targetY, newDir);
-				}
-				else if(map[targetY][targetX] == 2) {
+				//4.간다	
+				dfs(targetX, targetY, newDir);
+			}
+			//왼쪽 방향에 청소할 공간이 없다면
+			else {
+
+				int newnewDir = left(currentX, currentY, newDir);
+				System.out.println(newnewDir);
+				//네 방향 모두 청소가 이미 되어있지 않은 경우
+				if(search(currentX, currentY) == false) {
 					
-					if(isBlocked(targetX, targetY) == false) {
-						dfs(targetX, targetY, newDir);	
+					System.out.println("네 방향 모두 청소가 이미 되어있지 않은 경우");
+					System.out.println("(currentX, currentY, dir) ("+currentX+","+currentY+","+newnewDir+")");
+					dfs(currentX, currentY, newnewDir);
+					
+				} else {
+
+					int newTargetX = currentX + dx[goBack(newnewDir)];
+					int newTargetY = currentY + dy[goBack(newnewDir)];
+
+					if(map[newTargetY][newTargetY] == 1) {
+						//네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우
+						System.out.println("네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽");
+						return;
+
+					} else {
+						//네 방향 모두 청소가 이미 되어있거나 벽인 경우
+						//후진
+						System.out.println("후진");
+						dfs(newTargetX, newTargetY, newnewDir);
 					}
 
-					
 				}
-				else if(map[targetY][targetX] == 1) {
-					
-					if(isBlocked(targetX, targetY) == false) {
-						dfs(targetX, targetY, newDir);	
-					}
-					
-				}
-			}			
+
+
+			}
+		}			
 
 	}
 
-	//static boolean goBack(int currentX, int currentY) {
-		
-//	}
-	
+	static int goBack(int dir) {
+		if(dir == 0) {
+			return 2;
+		}
+		else if(dir ==1) {
+			return 3;
+		}
+		else if(dir == 2) {
+			return 4;
+		}
+		else {
+			return 1;
+		}
+	}
+
 	//네방향이 모두 청소||벽
-	static boolean isBlocked(int currentX, int currentY) {
+	static boolean search(int currentX, int currentY) {
 		for (int i = 0; i < 4; i++) {
 			int targetY = currentY + dy[i];
 			int targetX = currentX + dx[i];
-			
+
 			if(targetY >= 0 && targetY<N && targetX >= 0 && targetX <M) {
 				if(map[targetY][targetX] == 0) {
 					return false;
@@ -95,7 +126,7 @@ public class RobotCleaner14503 {
 		}
 		return true;
 	}
-	
+
 
 	//현재위치 청소
 	static void clean(int currentX, int currentY) {
