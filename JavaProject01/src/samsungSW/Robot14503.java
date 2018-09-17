2088144f27968f9c035156244ef3298b2b716eec
정소영 <starsuzi@naver.com>
2018-09-18 04:39:21 +0900

    final

--- JavaProject01/src/samsungSW/Robot14503.java
+++ JavaProject01/src/samsungSW/Robot14503.java
@@ -35,29 +35,29 @@ public class Robot14503 {
 
 
 
-		dfs(c,r,d,transfer(d));	
-		
+		dfs(c,r,transfer(d));	
+
 		int count = 0;
 		for (int y = 0; y < N; y++) {
 			for (int x = 0; x < M; x++) {
 				//	System.out.print(map[y][x]);
-				
+
 				if(map[y][x] == 2) {
 					count++;
 				}
 			}
 			//System.out.println();
 		}
-		
+
 		System.out.println(count);
 
 	}
 
-	static void dfs(int currentX, int currentY, int dir, int depth) {
+	static void dfs(int currentX, int currentY, int dir) {
 
-		System.out.println(dir);
+
 		dirCheck(dir);
-		System.out.println("(currentx, currenty) ("+currentY+","+currentX+")");
+	//	System.out.println("(currentx, currenty) ("+currentY+","+currentX+")");
 
 		//1.방문체크
 		if(map[currentY][currentX] == 0) {
@@ -66,62 +66,81 @@ public class Robot14503 {
 
 		for (int y = 0; y < N; y++) {
 			for (int x = 0; x < M; x++) {
-				System.out.print(map[y][x]);
+		//		System.out.print(map[y][x]);
 			}
-			System.out.println();
+		//	System.out.println();
 		}
 
 		//2.연결된길
-		for (int i = d+1; i < d+5; i++) {
-			int count = 0;
+		//int count = 4;
+		for (int i = dir+1; i < dir+5; i++) {
+
 			int targetY = currentY + dy[i%4];
 			int targetX = currentX + dx[i%4];
 			//3.갈수있는길
 			if(targetY>=0 && targetX>=0 && targetX<M && targetY<N) {
-				count++;
+				//System.out.println(count);
 				if(map[targetY][targetX] == 0) {
+					//count--;
 					//4.간다
-					dfs(targetX, targetY, i%4, depth+1);
+					dfs(targetX, targetY, i%4);
+					return;
 				}
 
-
-				//else {
-					if(count==3) {
-						//네방향 모두 벽이나 이미 청소
-						//후진가능
-						//해당이 안되어도 무조건 회전 조건 추가
-						int newtargetY = currentY + dy[((i%4)+2)%4];
-						int newtargetX = currentX + dx[((i%4)+2)%4];
-						
-						if(map[newtargetY][newtargetX] == 2) {
-							dfs(newtargetX, newtargetY, ((i%4)+2)%4, depth+1);		
-						}
-						//후진 불가능
-						else {
-							return;
-						}
-					}
-				//}
-
 			}
+		}
+		//if(count==4) {
+		//네방향 모두 벽이나 이미 청소
+		//후진가능
+	//	System.out.println("막힘");
+		int newtargetY = currentY + dy[(dir+2)%4];
+		int newtargetX = currentX + dx[(dir+2)%4];
+
+		if(map[newtargetY][newtargetX] == 2) {
+		//	System.out.println("후진가능");
+			dfs(newtargetX, newtargetY, dir);
 
 		}
+		//후진 불가능
+		else if(map[newtargetY][newtargetX] == 1) {
+		//	System.out.println("후진불가능");
+			return;
+		}
 	}
+	//}
+
+	static int left(int dir) {
+
+		if(dir == 1) {
+			return 2;
+		}
+		else if(dir == 3) {
+			return 0;
+		}
+		else if(dir == 2) {
+			return 3;
+		}
+		else {
+			return 1;
+		}
+
+	}
+
 	static int transfer(int dir) {
 		if(dir == 1) {
-			System.out.println("남");
+		//	System.out.println("남");
 			return 3;
 		}
 		else if(dir == 3) {
-			System.out.println("서");
+		//	System.out.println("서");
 			return 1;
 		}
 		else if(dir == 0) {
-			System.out.println("북");
+		//	System.out.println("북");
 			return 0;
 		}
 		else {
-			System.out.println("동");
+		//	System.out.println("동");
 			return 2;
 		}
 	}
@@ -129,16 +148,16 @@ public class Robot14503 {
 	static void dirCheck(int dir) {
 
 		if(dir == 2) {
-			System.out.println("남");
+		//	System.out.println("남");
 		}
 		else if(dir == 1) {
-			System.out.println("서");
+		//	System.out.println("서");
 		}
 		else if(dir == 0) {
-			System.out.println("북");
+		//	System.out.println("북");
 		}
 		else {
-			System.out.println("동");
+		//	System.out.println("동");
 		}
 
 	}
