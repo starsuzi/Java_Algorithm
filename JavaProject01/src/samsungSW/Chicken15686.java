86872526e306ec0f7b478e365fe562ac2a796863
정소영 <starsuzi@naver.com>
2018-10-14 02:56:38 +0900

    시간초과

--- JavaProject01/src/samsungSW/Chicken15686.java
+++ JavaProject01/src/samsungSW/Chicken15686.java
@@ -32,22 +32,19 @@ public class Chicken15686 {
 			}
 		}
 
-		//없어도 될듯
 		House = countHouse();
 
-		ChickenHouse = countChicken();
-
-		//얘는 필요
 		ChickenLocation = new ChickenPosition[M]; 
 		HouseLocation = new ChickenPosition[House];
 
-		chickDis = new int[M];
-		
+		chickDis = new int[House];
+
+		int count = 0;
 		for (int i = 1; i < N+1; i++) {
 			for (int j = 1; j < N+1; j++) {
 				if(map[i][j]==1) {
-					HouseLocation[i] = new ChickenPosition(i, j);
-					
+					HouseLocation[count] = new ChickenPosition(i, j);
+					count++;
 				}
 			}
 		}
@@ -55,12 +52,11 @@ public class Chicken15686 {
 		//print
 		for (int i = 1; i < N+1; i++) {
 			for (int j = 1; j < N+1; j++) {
-			//	System.out.print(map[i][j]);
+				//	System.out.print(map[i][j]);
 			}
-		//	System.out.println();
+			//	System.out.println();
 		}
 
-		//System.out.println(map[1][3]);
 		for (int y = 1; y < N+1; y++) {
 			for (int x = 1; x < N+1; x++) {
 				if(map[y][x] == 2) {
@@ -68,36 +64,43 @@ public class Chicken15686 {
 				}
 			}
 		}
-		
+
 		System.out.println(result);
-		
+
 	}
 
 	static void dfs(int currentY, int currentX, int depth) {
+		
 		//0. 도달?
 		if(depth == M-1) {
-			int temp ;
-			
-			ChickenPos = new ChickenPosition(currentY, currentX);
-			ChickenLocation[depth] = ChickenPos;
-			
-			temp = chickenDistance(ChickenLocation);
-			if(result>temp) {
-				temp = result;
-			}
 
+			int temp ;
+
+			ChickenLocation[depth] = new ChickenPosition(currentY, currentX);
+			temp = chickenDistance(ChickenLocation);
+			
+			if(result>temp) {
+				result = temp;
+			}
+			
+			System.out.println();
+			for (int i = 0; i < ChickenLocation.length; i++) {
+				System.out.print(ChickenLocation[i]);
+			}
+			return;
 		}
+
 		//1. 방문체크
-		visited[currentY][currentX] = 1; 
+		visited[currentY][currentX] = 1;
+		// 치킨점 좌표를 chickenLocation배열에 넣는다
+		ChickenLocation[depth] = new ChickenPosition(currentY, currentX);
 		//2. 연결된길
 		for (int y = 1; y < N+1; y++) {
 			for (int x = 1; x < N+1; x++) {
 				//3. 갈수있는길
 				if(map[y][x] == 2 && visited[y][x] != 1) {
-					//각 치킨점을 history배열에 넣는다
-					ChickenPos = new ChickenPosition(currentY, currentX);
-					ChickenLocation[depth] = ChickenPos;
 					//4. 간다
+				//	System.out.println("go");
 					dfs(y,x,depth+1);
 				}
 			}
@@ -119,22 +122,12 @@ public class Chicken15686 {
 		return count;
 	}
 
-	static int countChicken() {
-		int count = 0;
 
-		for (int y = 1; y < N+1; y++) {
-			for (int x = 1; x < N+1; x++) {
-				if(map[y][x] == 2) {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
 
 	static int chickenDistance(ChickenPosition[] chickLo) {
 		int temp;
 		int dis = Integer.MAX_VALUE;
+		int cnt = -1;
 
 		for (int i = 0; i < HouseLocation.length; i++) {
 			dis = Integer.MAX_VALUE;
@@ -142,6 +135,7 @@ public class Chicken15686 {
 				temp = (Math.abs(HouseLocation[i].Y -chickLo[j].Y)+Math.abs(HouseLocation[i].X -chickLo[j].X));
 				if(temp<dis) {
 					dis = temp;
+					cnt++;
 				}
 			}
 			chickDis[i] = dis;
